cd11915f28127aba6b36e8b303c2c7ab804b9edc
JuanCarlos11902 <105056958+JuanCarlos11902@users.noreply.github.com>
2023-10-29 20:20:55 +0100

    Menu Hecho

--- app/src/main/java/com/example/pizzeria/ActivityMenu.java
+++ app/src/main/java/com/example/pizzeria/ActivityMenu.java
@@ -2,7 +2,9 @@ package com.example.pizzeria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityMenu extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ActivityMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+    }
+
+    public void finalizarActividad(View view){
+        finish();
     }
 }
