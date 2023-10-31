fbacb32d07d742c7b11d4183e3d8fe0d66e27136
JuanCarlos11902 <105056958+JuanCarlos11902@users.noreply.github.com>
2023-10-31 13:45:22 +0100

    ReciclerView no va

--- app/src/main/java/com/example/pizzeria/ActivityCarta.java
+++ app/src/main/java/com/example/pizzeria/ActivityCarta.java
@@ -1,16 +1,27 @@
 package com.example.pizzeria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class ActivityCarta extends AppCompatActivity {
 
-
+    private RecyclerView recyclerView;
+    private PizzaAdapter pizzaAdapter;
+    private Servicio servicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carta);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        servicio = new Servicio();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        pizzaAdapter = new PizzaAdapter(servicio.getListaPizzasFabricadas());
+        recyclerView.setAdapter(pizzaAdapter);
     }
 }
--- app/src/main/java/com/example/pizzeria/PizzaAdapter.java
+++ app/src/main/java/com/example/pizzeria/PizzaAdapter.java
@@ -1,33 +1,77 @@
 package com.example.pizzeria;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class PizzaAdapter extends RecyclerView.Adapter<PizzaAdapter.ViewHolder> {
+import java.util.ArrayList;
+
+public class PizzaAdapter extends RecyclerView.Adapter<PizzaAdapter.PizzaViewHolder> {
+
+    private ArrayList<Pizza> pizzaList;
+
+    public PizzaAdapter(ArrayList<Pizza> pizzaList){
+        this.pizzaList = pizzaList;
+    }
+
     @NonNull
     @Override
-    public PizzaAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public PizzaAdapter.PizzaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_pizza, parent, false);
+        return new PizzaAdapter.PizzaViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PizzaAdapter.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull PizzaAdapter.PizzaViewHolder holder, int position) {
+        Pizza pizza = pizzaList.get(position);
+        holder.bind(pizza);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+       return pizzaList.size();
+    }
+
+    public class PizzaViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView txtNombre;
+        private TextView txtNumeroIngredientes;
+        private TextView txtIngredientes;
+        private TextView txtTamaño;
+        private TextView txtPrecio;
+
+
+        public PizzaViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtNombre = itemView.findViewById(R.id.txtNombre);
+            txtNumeroIngredientes = itemView.findViewById(R.id.txtNumero);
+            txtIngredientes = itemView.findViewById(R.id.txtIngredientes);
+            txtTamaño = itemView.findViewById(R.id.txtTamaño);
+            txtPrecio = itemView.findViewById(R.id.txtPrecio);
+        }
+
+        public void bind(Pizza pizza) {
+            txtNombre.setText(pizza.getNombre());
+            txtNumeroIngredientes.setText(pizza.getNumeroIngredientes());
+            txtIngredientes.setText(convertirArray(pizza));
+        }
+
+        public String convertirArray(Pizza pizza) {
+            String cadena = "";
+
+            for (int i = 0; i < pizza.getIngredientes().length; i++) {
+                cadena += pizza.getIngredientes()[i].toString();
+            }
+
+            return cadena;
+        }
+
     }
 }
 
-public class ViewHolder extends RecyclerView.ViewHolder{
 
-    TextView 
-    public ViewHolder(@NonNull View itemView) {
-        super(itemView);
-    }
-}
+
